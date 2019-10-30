9db891cc04a4813a8df2454ddc9dd37515845c2b
AiHughesToo <alex.hughes@interapthq.com>
2019-10-30 16:08:09 -0400

    reset password route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :authenticate_token!, except: [:create]
+  before_action :authenticate_token!, except: [:create, :reset_password]
 
   # GET /users
   def index
