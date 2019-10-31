c600b0927bdc4d626b5384abb72e40346c5fe92b
AiHughesToo <alex.hughes@interapthq.com>
2019-10-31 12:20:50 -0400

    reset password route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Mailer
-  Rails.application.routes.default_url_options[:host] = 'http://hug.interapt.com'
+  Rails.application.routes.default_url_options[:host] = 'http://aihughes.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
