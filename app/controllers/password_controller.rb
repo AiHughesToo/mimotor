b23864990baa7515686e5e1643ab018cd5950c32
AiHughesToo <alex.hughes@interapthq.com>
2019-11-01 13:34:22 -0400

    reset password route

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -6,11 +6,16 @@ class PasswordController < ApplicationController
     p params[:token]
     user = User.find_by_reset_password_token(token_reference)
       p user
-    if (user && user.save)
+    if (user)
+      user.password = params[:password]
+      user.reset_password_token = nil
+      user.reset_password_sent_at = nil
+      if (user.save)
         render json: {success: 'Password Reset'}
-      p user
+      else
+        render json: {errors: 'Email Not Found'}, status: :unprocessable_entity
+      end
     else
-      p "that did not work."
       render json: {errors: 'Email Not Found'}, status: :unprocessable_entity
     end
   end
