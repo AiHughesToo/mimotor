00ba465297fb052bbf8b398dc4819403a6c1276f
Alex Hughes <alex.hughes@interapthq.com>
2020-03-17 17:53:39 -0400

    update stats

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -112,7 +112,7 @@ class JobsController < ApplicationController
     p stat.life_t_distance + job_distance
     new_distance = stat.life_t_distance + job_distance
     # @current_user.stat.update(life_t_distance: job_distance + stat.life_t_distance)
-    stat.life_t_distance = 1
+    stat.life_t_distance = new_distance
     stat.save
     p @current_user.stat
   end
