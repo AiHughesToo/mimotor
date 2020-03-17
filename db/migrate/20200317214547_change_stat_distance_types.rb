4dac55c12904315ece9582e2cefeea0b536b2948
Alex Hughes <alex.hughes@interapthq.com>
2020-03-17 18:14:51 -0400

    update stats

--- db/migrate/20200317214547_change_stat_distance_types.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStatDistanceTypes < ActiveRecord::Migration[5.1]
-  def change
-    change_column :stats, :life_t_distance, :float
-    change_column :stats, :daily_distance, :float
-  end
-end
